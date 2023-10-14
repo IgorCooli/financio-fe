281a478d54143da7fb36a3d35bc9b95b255f58cb
igorcooli <igorcooli@gmail.com>
2023-10-14 17:20:30 -0300

    adjusting url

--- src/service/auth/AuthenticationService.js
+++ src/service/auth/AuthenticationService.js
@@ -5,7 +5,7 @@ import Cookies from 'js-cookie';
 const authenticate = async (username, password) => {
     try {
         //const response = await axios.post('https://financio-be-b0c1d25807f0.herokuapp.com/auth', {
-        const response = await axios.post('http://localhost:8080/auth', {
+        const response = await axios.post('https://financio-be-b0c1d25807f0.herokuapp.com/auth', {
             username: username,
             password: password,
         });
