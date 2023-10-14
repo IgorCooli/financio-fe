77ffc6a3928f86dbead950c87d89d150408e6008
igorcooli <igorcooli@gmail.com>
2023-10-14 18:46:17 -0300

    protecting routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/auth/Login';
 import HomePage from './components/home/HomePage';
+import ProtectedRoute from "./config/route/ProtectedRoute";
 
 function App() {
     return (
         <Router>
-            <Routes> {/* Use Routes here */}
-                <Route path="/home" element={<HomePage />} /> {/* Use element instead of component */}
+            <Routes>
                 <Route path="/" element={<Login />} />
+                <Route
+                    path="/home"
+                    element={<ProtectedRoute element={<HomePage />} />} // Use CustomRoute with element
+                />
             </Routes>
         </Router>
     );
