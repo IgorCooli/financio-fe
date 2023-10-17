dbf47eef5e70f3ba74a8bc81f0c36149d6f44e68
igorcooli <igorcooli@gmail.com>
2023-10-16 21:03:41 -0300

    adjusting login box

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,6 +2,7 @@ import {React, useState} from 'react';
 import { Container, Row, Col, Form, Button, Alert } from 'react-bootstrap';
 import { authenticate } from '../../service/auth/AuthenticationService';
 import { useNavigate } from 'react-router-dom';
+import '../../index.css';
 
 const Login = () => {
     const [username, setUsername] = useState('');
@@ -23,55 +24,57 @@ const Login = () => {
     }
 
     const containerStyle = {
-        backgroundColor: 'white'
+        backgroundColor: '#469867',
+        borderRadius: '10px',
+        width: '30rem',
+        height: '20rem',
+        justifyContent: 'center',
+        marginTop: '10rem',
+        padding: '5px'
     };
 
     return (
         <Container style={containerStyle}>
-            <Row className="justify-content-center align-items-center min-vh-100">
-                <Col xs={12} md={6}>
-                    <div className="text-center mb-4">
-                        <h1 style={{ color: 'purple' }}>Login</h1>
-                    </div>
-                    <Form>
-                        <Form.Group controlId="username" style={{ margin: '10px' }}>
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Enter your username"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                            />
-                        </Form.Group>
-                        <Form.Group controlId="password" style={{ margin: '10px' }}>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                placeholder="Enter your password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </Form.Group>
-                        {showAlert && (
-                            <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
-                                Wrong Credentials
-                            </Alert>
-                        )}
-                        <Row style={{ justifyContent: 'center', textAlign: 'center', margin: '15px' }}>
-                            <Col xs="auto">
-                                <Button variant="success" type="button" onClick={login}>
-                                    Sign In
-                                </Button>
-                            </Col>
-                            <Col xs="auto">
-                                <Button variant="warning" type="button" onClick={register}>
-                                    Register
-                                </Button>
-                            </Col>
-                        </Row>
-                    </Form>
-                </Col>
-            </Row>
+            <div className="text-center mb-4" style={{ margin: '15px' }}>
+                <h1 style={{ color: '#E6E6FA' }}>Login</h1>
+            </div>
+            <Form>
+                <Form.Group controlId="username" style={{ margin: '10px' }}>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter your username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group controlId="password" style={{ margin: '10px' }}>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                {showAlert && (
+                    <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
+                        Wrong Credentials
+                    </Alert>
+                )}
+                <Row style={{ justifyContent: 'center', textAlign: 'center', margin: '15px' }}>
+                    <Col xs="auto">
+                        <Button variant="primary" type="button" onClick={login}>
+                            Sign In
+                        </Button>
+                    </Col>
+                    <Col xs="auto">
+                        <Button variant="warning" type="button" onClick={register}>
+                            Register
+                        </Button>
+                    </Col>
+                </Row>
+            </Form>
         </Container>
     );
 };
