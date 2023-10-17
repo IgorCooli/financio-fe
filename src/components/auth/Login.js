63c431c8779948b425d2891b63e387daaf545bdd
igorcooli <igorcooli@gmail.com>
2023-10-16 23:45:27 -0300

    adjusting login page

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,5 @@
-import {React, useState} from 'react';
-import { Container, Row, Col, Form, Button, Alert } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Form, Button, Alert, Col, Row, InputGroup } from 'react-bootstrap';
 import { authenticate } from '../../service/auth/AuthenticationService';
 import { useNavigate } from 'react-router-dom';
 import '../../index.css';
@@ -8,6 +8,7 @@ const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [showAlert, setShowAlert] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
     const navigate = useNavigate();
 
     const login = async () => {
@@ -19,63 +20,128 @@ const Login = () => {
         }
     };
 
-    const register = () => {
-        navigate('/register')
-    }
+    const signUp = () => {
+        navigate('/register');
+    };
 
-    const containerStyle = {
-        backgroundColor: '#469867',
-        borderRadius: '10px',
-        width: '30rem',
-        height: '20rem',
+    const leftColumnStyle = {
+        backgroundColor: '#E6E6FA',
+        width: '70%',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
         justifyContent: 'center',
-        marginTop: '10rem',
-        padding: '5px'
+    };
+
+    const formStyle = {
+        color: '#469867',
+        width: '70%',
+    };
+
+    const rightColumnStyle = {
+        backgroundColor: '#469867',
+        width: '30%',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    };
+
+    const signUpMsg = {
+        color: '#E6E6FA',
+        margin: '5%',
+        justifyContent: 'center',
+    };
+
+    const registerButtonStyle = {
+        backgroundColor: '#E6E6FA',
+        color: '#469867',
+        padding: '10px 20px',
+        border: 'none',
+        cursor: 'pointer',
+        marginLeft: '25%',
+        width: '50%',
+    };
+
+    const signInButtonStyle = {
+        backgroundColor: '#469867',
+        color: 'white',
+        padding: '10px 20px',
+        border: 'none',
+        cursor: 'pointer',
+        margin: '5px',
     };
 
     return (
-        <Container style={containerStyle}>
-            <div className="text-center mb-4" style={{ margin: '15px' }}>
-                <h1 style={{ color: '#E6E6FA' }}>Login</h1>
+        <div style={{ display: 'flex' }}>
+            <div style={leftColumnStyle}>
+                <div className="text-center mb-4" style={{ color: '#469867' }}>
+                    <h1>Login</h1>
+                </div>
+                <Form style={formStyle}>
+                    <Form.Group controlId="username" style={{ margin: '10px' }}>
+                        <Form.Label>Username (Email)</Form.Label>
+                        <InputGroup>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter your email"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </InputGroup>
+                        <Form.Text className="text-muted">
+                            Please enter your email address as the username.
+                        </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="password" style={{ margin: '10px' }}>
+                        <Form.Label>Password</Form.Label>
+                        <InputGroup>
+                            <Form.Control
+                                type={showPassword ? 'text' : 'password'}
+                                placeholder="Enter your password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <Button
+                                variant="secondary"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? 'Hide' : 'Show'}
+                            </Button>
+                        </InputGroup>
+                    </Form.Group>
+                    {showAlert && (
+                        <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
+                            Wrong Credentials
+                        </Alert>
+                    )}
+                </Form>
+                <Button style={signInButtonStyle} variant="success" type="button" onClick={login}>
+                    Sign In
+                </Button>
             </div>
-            <Form>
-                <Form.Group controlId="username" style={{ margin: '10px' }}>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter your username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </Form.Group>
-                <Form.Group controlId="password" style={{ margin: '10px' }}>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Enter your password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </Form.Group>
-                {showAlert && (
-                    <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
-                        Wrong Credentials
-                    </Alert>
-                )}
-                <Row style={{ justifyContent: 'center', textAlign: 'center', margin: '15px' }}>
-                    <Col xs="auto">
-                        <Button variant="primary" type="button" onClick={login}>
-                            Sign In
+            <div style={rightColumnStyle}>
+                <Col>
+                    <Row>
+                        <h2 style={signUpMsg}>New Here?</h2>
+                    </Row>
+                    <Row>
+                        <h3 style={signUpMsg}>Sign up and discover a great amount of new opportunities</h3>
+                    </Row>
+                    <Row>
+                        <Button
+                            style={registerButtonStyle}
+                            variant="warning"
+                            type="button"
+                            onClick={signUp}
+                        >
+                            Sign Up
                         </Button>
-                    </Col>
-                    <Col xs="auto">
-                        <Button variant="warning" type="button" onClick={register}>
-                            Register
-                        </Button>
-                    </Col>
-                </Row>
-            </Form>
-        </Container>
+                    </Row>
+                </Col>
+            </div>
+        </div>
     );
 };
 
